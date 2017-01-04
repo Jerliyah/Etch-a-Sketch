457b64739fcfe6d1f397c01ce6d6b1082979c13d
Jerliyah <jerliyahcraig@gmail.com>
2017-01-04 16:43:03 -0600

    functionality complete. TODO: Find how to prevent div drag during drawing

--- action.js
+++ action.js
@@ -1,5 +1,27 @@
 
-
-for(var a = 0; a < 10; a++) {
-    $("section").append("<div></div>");
+// Fill in <main> with divs
+for(var a = 0; a < 5200; a++) {
+    $("main").append("<div></div>");
 }
+
+
+// Prevent default div drag
+$("div").ondragstart = function () { return false; };
+$("main").ondragstart = function () { return false; };
+
+
+// Clear board funtionality
+$("button").click(function() {
+    $("div").css({ "background":"transparent"});
+});
+
+
+// Mouse press and drag to draw
+$(window).mousedown( function() {
+    $("div").on("mouseenter", function() {
+        $(this).css({ "background":"black"});
+    });
+});
+$(window).mouseup( function() {
+    $("div").off();
+});
